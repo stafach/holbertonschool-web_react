d9475492f9f7000cd9bc019a3fdbb0767003d567
chermat mustapha <mchermat@outlook.fr>
2026-01-19 00:41:50 +0100

    add class StudentClass

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -21,3 +21,31 @@ const printTeacher: printTeacherFunction = (
 ): string => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
